22449cc85340c07f04bed6486b81c5474e1adc11
miwanoff <marina.iva.noffa@gmail.com>
2025-03-16 11:24:45 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,17 @@
+//let n = prompt("Input number of cats")
+
+// for (let i = 0; i < 3; i++) {
+//    document.write(i + "=^.^=" + "<br>");
+// }
+
+// let i = 0;
+// while (i < 3) {
+//   document.write(i + "=^.^=" + "<br>");
+//   i++;
+// }
+
+let i = 0;
+ do {
+  document.write(i + "=^.^=" + "<br>");
+  i++;
+} while (i < 3)
