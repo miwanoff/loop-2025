8e3c3bd50ce9341a07248875b1ab27b36438a787
miwanoff <marina.iva.noffa@gmail.com>
2025-03-16 11:34:37 +0200

    Add %

--- script.js
+++ script.js
@@ -12,6 +12,14 @@ let n = prompt("Input number of cats")
 //   i++;
 // } while (i < 3)
 
-for (let i = 0; i < n; i++) {
-   document.write(i + "=^.^=" + "<br>");
-}
+// for (let i = 0; i < n; i++) {
+//    document.write(i + "=^.^=" + "<br>");
+// }
+let k = 2;
+for (let i = 1; i <= n; i++) {
+    if(i % 2 == 0) {
+        document.write("<br>");
+    }
+    document.write("=^.^=");
+
+ }
